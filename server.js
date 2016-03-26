ba39ac2d1b0109959dea1f5afe00387854b72232
S K <xz64@users.noreply.github.com>
2016-03-25 22:29:00 -0400

    add server code

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var dateService = require('./src/dateService');
+
+var port = +process.env.PORT;
+var app = express();
+
+app.get('*', function(req, res) {
+  var query = decodeURI(req.originalUrl.slice(1));
+  var date = dateService.getJSON(query);
+  res.json(date);
+});
+
+app.listen(port);
